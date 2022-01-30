af42dafabdb708c0a033d29033c4f786046cc201
Madeleine Wagner <musomaddy@gmail.com>
2022-01-30 13:09:48 +1100

    Renaming of home to buckets.

--- app/src/main/java/com/example/simplebudget/ui/buckets/BucketsFragment.kt
+++ app/src/main/java/com/example/simplebudget/ui/buckets/BucketsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.simplebudget.ui.home
+package com.example.simplebudget.ui.buckets
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.simplebudget.R
-import com.example.simplebudget.databinding.FragmentHomeBinding
+import com.example.simplebudget.databinding.FragmentBucketsBinding
 
-class HomeFragment : Fragment() {
+class BucketsFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private lateinit var bucketsViewModel: BucketsViewModel
+    private var _binding: FragmentBucketsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+        bucketsViewModel =
+                ViewModelProvider(this).get(BucketsViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentBucketsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        bucketsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/simplebudget/ui/buckets/BucketsViewModel.kt
+++ app/src/main/java/com/example/simplebudget/ui/buckets/BucketsViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.simplebudget.ui.home
+package com.example.simplebudget.ui.buckets
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class BucketsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is where the buckets should go"
